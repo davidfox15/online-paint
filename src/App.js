ce564eeb27b1ba7f04c87faf07f842298a0846be
David Lisitsyn <davidfox15@yandex.ru>
2023-02-06 17:35:31 +0300

    add react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,15 +1,49 @@
 import SettingBar from './components/SettingsBar'
 import Toolbar from './components/Toolbar'
 import Canvas from './components/Canvas'
+import {
+  Route,
+  Routes,
+  createBrowserRouter,
+  BrowserRouter,
+  Navigate,
+} from 'react-router-dom'
 import './styles/app.scss'
 
-function App() {
+const router = createBrowserRouter([
+  {
+    path: '/:id',
+    element: <div className="app"></div>,
+  },
+  {},
+])
+
+function MainPage() {
   return (
-    <div className="app">
+    <>
       <Toolbar />
       <SettingBar />
       <Canvas />
-    </div>
+    </>
+  )
+}
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/:id" element={<MainPage />}></Route>
+        <Route
+          path="*"
+          element={
+            <Navigate
+              to={`/f${(+new Date()).toString(16)}`}
+              replace
+            />
+          }
+        ></Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
